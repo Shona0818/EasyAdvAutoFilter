67302f5afae8a92117789b6668d3627671d4060f
Shona0818 <shonakashyap200@gmail.com>
2024-06-01 22:51:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhithemachinegun/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/Shona0818/EasyAdvAutoFilter.git /EasyAdvAutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EasyAdvAutoFilter
